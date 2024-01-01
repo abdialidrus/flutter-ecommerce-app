d373fe06a54d8d9bead1004b174855cbc1f2085e
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-01-01 23:29:27 +0700

    build login screen UI

---
+++ lib/common/widgets/login_signup/social_buttons.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/utils/constants/colors.dart';
+import 'package:flutter_ecommerce_app/utils/constants/image_strings.dart';
+import 'package:flutter_ecommerce_app/utils/constants/sizes.dart';
+
+class TSocialButtons extends StatelessWidget {
+  const TSocialButtons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: TColors.grey),
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: const Image(
+            width: TSizes.iconMd,
+            height: TSizes.iconMd,
+            image: AssetImage(TImages.google),
+          ),
+        ),
+        const SizedBox(width: TSizes.spaceBtwItems),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: TColors.grey),
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: const Image(
+            width: TSizes.iconMd,
+            height: TSizes.iconMd,
+            image: AssetImage(TImages.facebook),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/authentication/controllers/onboarding/onboarding_controller.dart
+++ lib/features/authentication/controllers/onboarding/onboarding_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/features/authentication/screens/login/login.dart';
 import 'package:get/get.dart';
 
 class OnBoardingController extends GetxController {
@@ -20,7 +21,7 @@ class OnBoardingController extends GetxController {
   // Update current index & jump to the next page
   void nextPage() {
     if (currentPageIndex.value == 2) {
-      //Get.to(LoginScreen())
+      Get.offAll(const LoginScreen());
     } else {
       int page = currentPageIndex.value + 1;
       pageController.jumpToPage(page);

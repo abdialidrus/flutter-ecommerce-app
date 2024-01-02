2fa86760358deecff5ff2f2103ecfcbf23a1c048
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-01-02 23:55:41 +0700

    update home screen

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -3,6 +3,8 @@ import 'package:flutter_ecommerce_app/common/widgets/custom_shapes/containers/pr
 import 'package:flutter_ecommerce_app/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:flutter_ecommerce_app/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:flutter_ecommerce_app/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:flutter_ecommerce_app/features/shop/screens/home/widgets/promo_slider.dart';
+import 'package:flutter_ecommerce_app/utils/constants/image_strings.dart';
 import 'package:flutter_ecommerce_app/utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -27,6 +29,18 @@ class HomeScreen extends StatelessWidget {
 
                   /// Categories
                   THomeCategories(),
+                ],
+              ),
+            ),
+
+            /// Body
+            Padding(
+              padding: EdgeInsets.all(TSizes.defaultSpace),
+              child: TPromoSlider(
+                banners: [
+                  TImages.promoBanner1,
+                  TImages.promoBanner2,
+                  TImages.promoBanner3,
                 ],
               ),
             ),

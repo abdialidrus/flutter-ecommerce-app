ca7bb6165c6c501b5db160b3410542777a5a97d1
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-01-04 21:13:53 +0700

    setup product detail screen

---
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
+import 'package:flutter_ecommerce_app/features/shop/screens/product_details/widgets/product_meta_data.dart';
+import 'package:flutter_ecommerce_app/features/shop/screens/product_details/widgets/rating_share_widget.dart';
+import 'package:flutter_ecommerce_app/utils/constants/sizes.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  const ProductDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            /// Product Image Slider
+            TProductImageSlider(),
+
+            /// Product Details
+            Padding(
+              padding: EdgeInsets.only(
+                right: TSizes.defaultSpace,
+                left: TSizes.defaultSpace,
+                bottom: TSizes.defaultSpace,
+              ),
+              child: Column(
+                children: [
+                  /// Rating & Share
+                  TRatingAndShare(),
+
+                  /// Price, Title, Stock & Brand
+                  TProductMetaData(),
+
+                  /// Attributes
+                  /// Checkout Button
+                  /// Description
+                  /// Reviews
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

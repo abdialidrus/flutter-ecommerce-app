fc02969e678b2e39014e22cbde66f11b1c366d83
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-01-09 15:44:49 +0700

    use sortable products

--- lib/features/shop/screens/all_products/all_products.dart
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_app/common/widgets/appbar/appbar.dart';
-import 'package:flutter_ecommerce_app/common/widgets/layouts/grid_layout.dart';
-import 'package:flutter_ecommerce_app/common/widgets/products/product_cards/product_card_vertical.dart';
+import 'package:flutter_ecommerce_app/common/widgets/products/sortable/sortable_products.dart';
 import 'package:flutter_ecommerce_app/utils/constants/sizes.dart';
-import 'package:iconsax/iconsax.dart';
 
 class AllProductsScreen extends StatelessWidget {
   const AllProductsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       appBar: TAppBar(
         title: Text('Popular Products'),
         showBackArrow: true,
@@ -18,37 +16,7 @@ class AllProductsScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(TSizes.defaultSpace),
-          child: Column(
-            children: [
-              /// Dropdown
-              DropdownButtonFormField(
-                decoration:
-                    const InputDecoration(prefixIcon: Icon(Iconsax.sort)),
-                onChanged: (value) {},
-                items: [
-                  'Name',
-                  'Higher price',
-                  'Lower price',
-                  'Sale',
-                  'Newest',
-                  'Popularity'
-                ]
-                    .map(
-                      (option) => DropdownMenuItem(
-                        value: option,
-                        child: Text(option),
-                      ),
-                    )
-                    .toList(),
-              ),
-              const SizedBox(height: TSizes.spaceBtwSections),
-
-              /// Products
-              TGridLayout(
-                  itemCount: 8,
-                  itemBuilder: (_, index) => const TProductCardVertical()),
-            ],
-          ),
+          child: TSortableProducts(),
         ),
       ),
     );

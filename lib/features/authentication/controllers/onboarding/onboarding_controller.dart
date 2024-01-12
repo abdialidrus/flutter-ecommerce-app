510b4748d4e962843272d36dfb455c93f3be5c2c
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-01-12 08:44:30 +0700

    update isFirstTime value to false as the user completed the onboarding screen

--- lib/features/authentication/controllers/onboarding/onboarding_controller.dart
+++ lib/features/authentication/controllers/onboarding/onboarding_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_app/features/authentication/screens/login/login.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class OnBoardingController extends GetxController {
   static OnBoardingController get instance => Get.find();
@@ -21,6 +22,8 @@ class OnBoardingController extends GetxController {
   // Update current index & jump to the next page
   void nextPage() {
     if (currentPageIndex.value == 2) {
+      final storage = GetStorage();
+      storage.write('isFirstTime', false);
       Get.offAll(() => const LoginScreen());
     } else {
       int page = currentPageIndex.value + 1;

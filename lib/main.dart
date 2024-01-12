a2e27dd645a7f4c8b3f041b1e09c035cbf5a273a
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-01-12 08:45:00 +0700

    update main func

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_app/app.dart';
+import 'package:flutter_ecommerce_app/data/repositories/authentication/authentication_repository.dart';
+import 'package:flutter_ecommerce_app/firebase_options.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+Future<void> main() async {
+  /// Widgets binding
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+
+  /// Get storage init
+  await GetStorage.init();
+
+  /// await the splash screen until other items are loaded
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  /// Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+
   runApp(const App());
 }

ef73f2e3dd4992dd6d906c58ae367bd199598ced
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-01-17 18:21:12 +0700

    call logout function

--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -4,6 +4,7 @@ import 'package:flutter_ecommerce_app/common/widgets/custom_shapes/containers/pr
 import 'package:flutter_ecommerce_app/common/widgets/list_tiles/settings_menu_tile.dart';
 import 'package:flutter_ecommerce_app/common/widgets/list_tiles/user_profile_tile.dart';
 import 'package:flutter_ecommerce_app/common/widgets/texts/section_heading.dart';
+import 'package:flutter_ecommerce_app/data/repositories/authentication/authentication_repository.dart';
 import 'package:flutter_ecommerce_app/features/personalization/screens/address/address.dart';
 import 'package:flutter_ecommerce_app/features/personalization/screens/profile/profile.dart';
 import 'package:flutter_ecommerce_app/features/shop/screens/order/order.dart';
@@ -135,8 +136,9 @@ class SettingsScreen extends StatelessWidget {
                   SizedBox(
                     width: double.infinity,
                     child: OutlinedButton(
-                      onPressed: () {},
-                      child: const Text('Button'),
+                      onPressed: () =>
+                          AuthenticationRepository.instance.logout(),
+                      child: const Text('Logout'),
                     ),
                   ),
                   const SizedBox(height: TSizes.spaceBtwSections * 2.5),

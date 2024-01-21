d36da6823b4caf3ff57b89f3ae6fda2e52a144a0
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2024-01-21 18:02:41 +0700

    add Shimmer library

---
+++ lib/common/widgets/loaders/shimmer.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/utils/constants/colors.dart';
+import 'package:flutter_ecommerce_app/utils/helpers/helper_functions.dart';
+import 'package:shimmer/shimmer.dart';
+
+class TShimmerEffect extends StatelessWidget {
+  const TShimmerEffect({
+    super.key,
+    required this.width,
+    required this.height,
+    this.radius = 15,
+    this.color,
+  });
+
+  final double width, height, radius;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode = THelperFunctions.isDarkMode(context);
+
+    return Shimmer.fromColors(
+      baseColor: isDarkMode ? Colors.grey[850]! : Colors.grey[300]!,
+      highlightColor: isDarkMode ? Colors.grey[700]! : Colors.grey[100]!,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: color ?? (isDarkMode ? TColors.darkerGrey : TColors.white),
+          borderRadius: BorderRadius.circular(radius),
+        ),
+      ),
+    );
+  }
+}

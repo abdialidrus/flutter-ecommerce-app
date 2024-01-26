f20275101a11c92df8b993698e210fd960e51815
Muhammad Abdi Alidrus <113153953+abdimuhammad@users.noreply.github.com>
2024-01-26 23:47:30 +0700

    create Banner model

---
+++ lib/features/shop/models/banner_model.dart
@@ -0,0 +1,11 @@
+class BannerModel {
+  String imageUrl;
+  String targetScreen;
+  bool active;
+
+  BannerModel({
+    required this.imageUrl,
+    required this.targetScreen,
+    required this.active,
+  });
+}

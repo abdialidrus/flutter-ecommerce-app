29fb26753237e0d6d61df89c67c14f0a761468c0
Muhammad Abdi Alidrus <113153953+abdimuhammad@users.noreply.github.com>
2024-01-26 23:47:13 +0700

    add CategoryModel as dependency

--- lib/features/shop/screens/store/widgets/category_tab.dart
+++ lib/features/shop/screens/store/widgets/category_tab.dart
@@ -3,11 +3,14 @@ import 'package:flutter_ecommerce_app/common/widgets/brands/brand_show_case.dart
 import 'package:flutter_ecommerce_app/common/widgets/layouts/grid_layout.dart';
 import 'package:flutter_ecommerce_app/common/widgets/products/product_cards/product_card_vertical.dart';
 import 'package:flutter_ecommerce_app/common/widgets/texts/section_heading.dart';
+import 'package:flutter_ecommerce_app/features/shop/models/category_model.dart';
 import 'package:flutter_ecommerce_app/utils/constants/image_strings.dart';
 import 'package:flutter_ecommerce_app/utils/constants/sizes.dart';
 
 class TCategoryTab extends StatelessWidget {
-  const TCategoryTab({super.key});
+  const TCategoryTab({super.key, required this.category});
+
+  final CategoryModel category;
 
   @override
   Widget build(BuildContext context) {

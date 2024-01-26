5a119dd3af2f5a79b10a66decfe83e2c3d0363a8
Muhammad Abdi Alidrus <113153953+abdimuhammad@users.noreply.github.com>
2024-01-26 23:44:59 +0700

    observe category data in the controller and update category list UI accordingly

--- lib/features/shop/screens/home/widgets/home_categories.dart
+++ lib/features/shop/screens/home/widgets/home_categories.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_app/common/widgets/image_text_widgets/vertical_image_text.dart';
+import 'package:flutter_ecommerce_app/common/widgets/shimmers/category_shimmer.dart';
+import 'package:flutter_ecommerce_app/features/shop/controllers/category_controller.dart';
 import 'package:flutter_ecommerce_app/features/shop/screens/sub_category/sub_categories.dart';
-import 'package:flutter_ecommerce_app/utils/constants/image_strings.dart';
 import 'package:get/get.dart';
 
 class THomeCategories extends StatelessWidget {
@@ -11,25 +12,34 @@ class THomeCategories extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        /// Categories
-        SizedBox(
+    final categoryController = Get.put(CategoryController());
+
+    return Obx(
+      () {
+        if (categoryController.isLoading.value) return const TCategoryShimmer();
+
+        if (categoryController.featuredCategories.isEmpty) {
+          return Center(
+            child: Text('No Data Found', style: Theme.of(context).textTheme.bodyMedium!.apply(color: Colors.white)),
+          );
+        }
+        return SizedBox(
           height: 80,
           child: ListView.builder(
             shrinkWrap: true,
-            itemCount: 6,
+            itemCount: categoryController.featuredCategories.length,
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) {
+              final category = categoryController.featuredCategories[index];
               return TVerticalImageText(
-                image: TImages.shoeIcon,
-                title: 'Shoes',
-                onTap: () => Get.to(() => const SubCategoriesScreen()),
+                image: category.image,
+                title: category.name,
+                onTap: () => Get.to(() => SubCategoriesScreen()),
               );
             },
           ),
-        )
-      ],
+        );
+      },
     );
   }
 }

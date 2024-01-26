a5d57210f7126effec2155dc77a4014015674d5a
Muhammad Abdi Alidrus <113153953+abdimuhammad@users.noreply.github.com>
2024-01-26 23:38:27 +0700

    use TCircularImage for image

--- lib/common/widgets/image_text_widgets/vertical_image_text.dart
+++ lib/common/widgets/image_text_widgets/vertical_image_text.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/common/widgets/images/t_circular_image.dart';
 import 'package:flutter_ecommerce_app/utils/constants/colors.dart';
 import 'package:flutter_ecommerce_app/utils/constants/sizes.dart';
 import 'package:flutter_ecommerce_app/utils/helpers/helper_functions.dart';
@@ -11,17 +12,17 @@ class TVerticalImageText extends StatelessWidget {
     this.textColor = TColors.white,
     this.backgroundColor,
     this.onTap,
+    this.isNetworkImage = true,
   });
 
   final String image, title;
   final Color textColor;
   final Color? backgroundColor;
+  final bool isNetworkImage;
   final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    final dark = THelperFunctions.isDarkMode(context);
-
     return GestureDetector(
       onTap: onTap,
       child: Padding(
@@ -29,28 +30,18 @@ class TVerticalImageText extends StatelessWidget {
         child: Column(
           children: [
             /// Circular icon
-            Container(
-              width: 56,
-              height: 56,
-              padding: const EdgeInsets.all(TSizes.sm),
-              decoration: BoxDecoration(
-                color:
-                    backgroundColor ?? (dark ? TColors.black : TColors.white),
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: Center(
-                child: Image(
-                  image: AssetImage(image),
-                  fit: BoxFit.cover,
-                  color: dark ? TColors.light : TColors.dark,
-                ),
-              ),
+            TCircularImage(
+              image: image,
+              fit: BoxFit.fitWidth,
+              padding: TSizes.sm * 1.4,
+              isNetworkImage: isNetworkImage,
+              backgroundColor: backgroundColor,
+              overlayColor: THelperFunctions.isDarkMode(context) ? TColors.light : TColors.dark,
             ),
 
             /// Text
             const SizedBox(height: TSizes.spaceBtwItems / 2),
             SizedBox(
-              width: 55,
               child: Text(
                 title,
                 style: Theme.of(context).textTheme.labelMedium!.apply(

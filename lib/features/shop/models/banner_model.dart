09676555f83959fa37d4a5decd47b6ab5d0c0239
Muhammad Abdi Alidrus <113153953+abdimuhammad@users.noreply.github.com>
2024-01-27 15:53:45 +0700

    update Banner model

--- lib/features/shop/models/banner_model.dart
+++ lib/features/shop/models/banner_model.dart
@@ -1,11 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class BannerModel {
   String imageUrl;
-  String targetScreen;
-  bool active;
+  final String targetScreen;
+  final bool active;
 
   BannerModel({
     required this.imageUrl,
     required this.targetScreen,
     required this.active,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'imageUrl': imageUrl,
+      'targetScreen': targetScreen,
+      'active': active,
+    };
+  }
+
+  factory BannerModel.fromSnapshot(DocumentSnapshot snapshot) {
+    final data = snapshot.data() as Map<String, dynamic>;
+    return BannerModel(
+      imageUrl: data['imageUrl'] ?? '',
+      targetScreen: data['targetScreen'] ?? '',
+      active: data['active'] ?? '',
+    );
+  }
 }

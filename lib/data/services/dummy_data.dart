497a34b62f413b8aaac085b3afc99a3113f3739e
Muhammad Abdi Alidrus <113153953+abdimuhammad@users.noreply.github.com>
2024-01-27 15:53:33 +0700

    update dummy banner list data

--- lib/data/services/dummy_data.dart
+++ lib/data/services/dummy_data.dart
@@ -1,17 +1,18 @@
 import 'package:flutter_ecommerce_app/features/shop/models/banner_model.dart';
 import 'package:flutter_ecommerce_app/features/shop/models/category_model.dart';
+import 'package:flutter_ecommerce_app/routes/routes.dart';
 import 'package:flutter_ecommerce_app/utils/constants/image_strings.dart';
 
 class TDummyData {
   /// -- Banners
   static final List<BannerModel> banners = [
-    BannerModel(imageUrl: TImages.banner1, targetScreen: 'TRoutes.order', active: false),
-    BannerModel(imageUrl: TImages.banner2, targetScreen: 'TRoutes.cart', active: true),
-    BannerModel(imageUrl: TImages.banner3, targetScreen: 'TRoutes.favourites', active: true),
-    BannerModel(imageUrl: TImages.banner4, targetScreen: 'TRoutes.search', active: true),
-    BannerModel(imageUrl: TImages.banner5, targetScreen: 'TRoutes.settings', active: true),
-    BannerModel(imageUrl: TImages.banner6, targetScreen: 'TRoutes.userAddress', active: true),
-    BannerModel(imageUrl: TImages.banner8, targetScreen: 'TRoutes.checkout', active: false),
+    BannerModel(imageUrl: TImages.banner1, targetScreen: TRoutes.order, active: false),
+    BannerModel(imageUrl: TImages.banner2, targetScreen: TRoutes.cart, active: true),
+    BannerModel(imageUrl: TImages.banner3, targetScreen: TRoutes.favourites, active: true),
+    BannerModel(imageUrl: TImages.banner4, targetScreen: TRoutes.search, active: true),
+    BannerModel(imageUrl: TImages.banner5, targetScreen: TRoutes.settings, active: true),
+    BannerModel(imageUrl: TImages.banner6, targetScreen: TRoutes.userAddress, active: true),
+    BannerModel(imageUrl: TImages.banner8, targetScreen: TRoutes.checkout, active: false),
   ];
 
   /// -- List of all Categories
